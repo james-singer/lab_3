68fdf1d3f5fe97b9e3fd4b550e792a1d6a779b29
James-Singer <james.singer@colorado.edu>
2020-02-20 13:25:54 -0700

    the next one

--- lab3_script.sh
+++ lab3_script.sh
@@ -4,9 +4,22 @@
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
-echo "insert a file name"
-read filename
-echo "insert regex"
-read regex
-grep $regex $filename
-grep -c '[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}'  $filename 
+#echo "insert a file name"
+#read filename
+#echo "insert regex"
+#read regex
+
+#Problem 2 Code:
+#grep $regex $filename
+
+#Problem 3 Code:
+#egrep -c "^[1-9]{3}-[1-9]{3}-[1-9]{4}$" regex_practice.txt
+
+#problem 4.1 Code:
+#egrep -c "^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" regex_practice.txt
+
+#problem 4.2 Code:
+#egrep "^303" regex_practice.txt
+
+#Problem 4.3 Code:
+egrep "geocities.com$" regex_practice.txt >> email_results.txt
